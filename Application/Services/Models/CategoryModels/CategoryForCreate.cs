9aa638bf1679ead801267975bd70047c2cd1740f
TuanVu <hero0979144@gmail.com>
2024-07-04 14:19:52 +0700

    fecth brach dev

---
+++ Application/Services/Models/CategoryModels/CategoryForCreate.cs
@@ -0,0 +1,25 @@
+﻿using Application.Services.Contracts.Repositories;
+using Application.Services.Models.CategoryModels.Base;
+using FluentValidation;
+
+namespace Application.Services.Models.CategoryModels
+{
+    public class CategoryForCreate : CategoryBaseDto
+    {
+        public Dictionary<string, object> Errors { get; set; } = new Dictionary<string, object>();
+    }
+    public class CategoryForCreateValidator : AbstractValidator<CategoryForCreate>
+    {
+        private readonly ICategoryRepository _categoryRepo;
+        public CategoryForCreateValidator(ICategoryRepository categoryRepo)
+        {
+            _categoryRepo = categoryRepo;
+
+            //RuleFor(x => x.Price).NotEqual(0).WithMessage("Không được bằng 0");
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Bắt buộc phải có")
+                .NotNull().WithMessage("Không được để trống")
+                .MustAsync(categoryRepo.IsUniqueCategoryName).WithMessage("Thể loại này đã tồn tại");
+        }
+    }
+}
